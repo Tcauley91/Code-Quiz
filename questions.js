5d135ac14f8ba8f241ae54fd7ef6f99ad8559123
brandon cauley <cauleytyler@gmail.com>
2019-11-09 21:51:27 -0500

    questions script added

---
+++ questions.js
@@ -0,0 +1,23 @@
+// create our questions
+let questions = [
+  {
+      question : "What does HTML stand for?",
+      choiceA : "Correct",
+      choiceB : "Wrong",
+      choiceC : "Wrong",
+      correct : "A"
+  },{
+      question : "What does CSS stand for?",
+      choiceA : "Wrong",
+      choiceB : "Correct",
+      choiceC : "Wrong",
+      correct : "B"
+  },{
+      question : "What does JS stand for?",
+      choiceA : "Wrong",
+      choiceB : "Wrong",
+      choiceC : "Correct",
+      correct : "C"
+  }
+];
+  
